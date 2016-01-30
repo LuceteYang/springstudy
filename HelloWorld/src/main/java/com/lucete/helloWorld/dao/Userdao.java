c0ba97ef7307f4224897a4d8fe3bf8ba841b92f2
rooney11 <jae6120@naver.com>
2016-01-30 16:27:43 +0900

    first

---
+++ HelloWorld/src/main/java/com/lucete/helloWorld/dao/Userdao.java
@@ -0,0 +1,11 @@
+package com.lucete.helloWorld.dao;
+
+import javax.activation.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Userdao {
+	@Autowired
+	private DataSource dataSource;
+	
+}
